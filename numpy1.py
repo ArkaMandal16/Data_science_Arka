002a6b5212084b5cd96885161db2ccef66b0e3bd
Arka Mandal <arkamandal764@gmail.com>
2024-03-10 00:42:45 +0530

    Completed upto numpy array slicing

--- numpy1.py
+++ numpy1.py
@@ -46,3 +46,58 @@ print(c.dtype)
 print(a.transpose())
 print(b.transpose())
 print(c.transpose())
+
+# empty function : creates empty array of random data: print(np.empty((row,col), dtype = blahblah))
+
+print(np.empty((1,2), dtype = int))
+
+# ones function: np.ones((row,col), dtype = blahvlah):prints only one element in places = 1
+# default data type is float
+x = np.ones((3,3))
+print(x)
+
+y= np.ones((3,4), dtype = int)
+print(y)
+
+#zeros function
+
+y= np.zeros((3,4), dtype = bool)
+print(y)
+
+#arange function: np.arange(start,end,step): works kinda like a for loop
+
+print(np.arange(1,10,2))
+
+# reshape function: arr.reshape((row,col))
+
+print(b.reshape(3,2))
+
+# flatten function : inverse of reshape
+# it returns copy of original array. if anyvalues are modified in this array, the original array is not affected.
+
+print(b.flatten())
+
+#ravel function : same work as flatten but different functionality. if anyvalues are modified in this array, the original array is affected.
+
+print(b.ravel())
+
+# numpy array slicing
+
+arr = np.arange(1,21)
+arr = arr.reshape(5,4)
+print(arr)
+
+print(arr[1])
+#output = [5 6 7 8]
+print(arr[2,3])
+#output = 12
+print(arr[1:3])
+#output = [[ 5  6  7  8]
+#           [ 9 10 11 12]]
+print(arr[:2])
+#output = [[1 2 3 4]
+#         [5 6 7 8]]
+print(arr[:,2])
+#OUTPUT = [ 3  7 11 15 19]
+print(arr[1:3 , 2])
+#output= [ 7 11]
