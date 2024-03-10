db822d088ae9c41145d7e402a7ccf1155e8c10d0
Arka Mandal <arkamandal764@gmail.com>
2024-03-11 02:41:01 +0530

    learned pandas

--- pandas1.py
+++ pandas1.py
@@ -1,3 +1,19 @@
+from operator import index
+from os import name
 import pandas as pd
 
-pd.__version__
+#pd.__version__
+
+#pandas series
+
+lst = [1,2,3]
+series = pd.Series(lst)
+print(series)
+
+a = pd.Series([1,2,3,4], index=['a','b','c','d'], name='random')
+print(a)
+
+#pandas series using dictionary
+
+dict = pd.Series({'a':1,'b':2,'c':3})
+print(dict)
